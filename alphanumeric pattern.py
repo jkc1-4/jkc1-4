203d61b29c06753350b849180d11a0cd38fcb135
Jahnavi chauhan <chauhankhushi.16775@gmail.com>
2023-10-20 13:57:13 +0530

    Create alphanumeric pattern.py

---
+++ alphanumeric pattern.py
@@ -0,0 +1,8 @@
+# Input a string
+input_string = input("Enter a string: ")
+
+# Check if the string is alphanumeric
+if input_string.isalnum():
+    print("The input string is alphanumeric.")
+else:
+    print("The input string is not alphanumeric.")
